9cc5a548e318a29eef0d38a0211afe334a1e2206
upfrog <srr@routhouse.org>
2024-04-14 22:24:43 -0400

    Everything is awful

--- src/main/java/Models/UserModel.java
+++ src/main/java/Models/UserModel.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class UserModel
 {
-	String displayName;
-	String job;
-	String company;
-	String description;
+	public String displayName;
+	public String job;
+	public String company;
+	public String description;
 	ArrayList<UserModel> following = new ArrayList<UserModel>();
 	ArrayList<UserModel> followers = new ArrayList<UserModel>();
 	
---
+++ src/main/java/Views/UserPageViewController.java
@@ -0,0 +1,58 @@
+package Views;
+
+import Models.ViewTransitionalModelInterface;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import Models.UserModel;
+
+
+public class UserPageViewController
+{
+	ViewTransitionalModelInterface model;
+	
+	public void setModel(ViewTransitionalModelInterface newModel)
+	{
+		model = newModel;
+	}
+	
+	
+	private TextField UserPageViewName;
+	@FXML
+	void fillUserPage(UserModel user)
+	{
+		UserPageViewName.setText("Billy Joe Bob Joe McFace");
+	}
+	
+	
+	
+	
+	/**
+	@FXML 
+	void showUserPage()
+	{
+		model.showUserPageView();
+	}
+	
+	@FXML
+	void onClickHome(ActionEvent event)
+	{
+		System.out.println("Home clicked");
+	}
+	
+	@FXML
+	void onClickFeed(ActionEvent event)
+	{
+		System.out.println("Profile clicked");
+	}
+	
+	@FXML
+	void onClickProfile(ActionEvent event)
+	{
+		System.out.println("Profile clicked");
+	}
+	
+	**/
+	
+	
+}
